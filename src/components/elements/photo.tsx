5be9f7327ee2c5498c8c6705037f5ce612d319dd
aalend <aalendemirov@gmail.com>
2023-02-19 16:36:51 +0100

    feat: Contentful integration. Getting content for each page form CMS.

--- src/components/elements/photo.tsx
+++ src/components/elements/photo.tsx
@@ -1,11 +1,20 @@
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
 
 type PhotoProps = {
-	src: StaticImageData;
+	src: string;
 	alt: string;
 	className?: string;
 };
 
 export default function Photo({ src, alt, className }: PhotoProps) {
-	return <Image className={`${className ?? ''}`} src={src} alt={alt} priority={true} />;
+	return (
+		<Image
+			className={`${className ?? ''}`}
+			src={src}
+			alt={alt}
+			priority={true}
+			width={736}
+			height={441}
+		/>
+	);
 }
