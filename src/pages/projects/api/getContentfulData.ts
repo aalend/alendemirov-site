10b875264413c458518b024c5271c0a4ad958dde
aalend <aalendemirov@gmail.com>
2023-02-21 20:53:29 +0100

    fix: project components types

--- src/pages/projects/api/getContentfulData.ts
+++ src/pages/projects/api/getContentfulData.ts
@@ -1,19 +1,18 @@
-const CONTENTFUL_URL = `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_JS_CONTENTFUL_SPACE_ID}/environments/master`;
-
-const headers = {
-	Authorization: `Bearer ${process.env.NEXT_JS_CONTENTFUL_DELIVERY_TOKEN}`,
-	'Content-Type': 'application/json',
-};
-
 export default async function getContentfulData(query: string, variables = {}) {
-	const res = await fetch(CONTENTFUL_URL, {
-		method: 'POST',
-		headers,
-		body: JSON.stringify({
-			query,
-			variables,
-		}),
-	});
+	const res = await fetch(
+		`https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_JS_CONTENTFUL_SPACE_ID}/environments/master`,
+		{
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${process.env.NEXT_JS_CONTENTFUL_DELIVERY_TOKEN}`,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				query,
+				variables,
+			}),
+		}
+	);
 
 	if (!res.ok) throw Error(`Cannot get data from ContentfulURL. - ${res.status}`);
 
