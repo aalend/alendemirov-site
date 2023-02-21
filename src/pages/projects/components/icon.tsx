28bc82e7150ac7a974643a06dfa3f7938a75b458
aalend <aalendemirov@gmail.com>
2023-02-21 20:42:38 +0100

    fix: Icon src and alt has any type.

--- src/pages/projects/components/icon.tsx
+++ src/pages/projects/components/icon.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-export default function Icon({ src, alt }) {
+export default function Icon({ src, alt }: {src:string, alt: string}) {
 	return (
 		<div className='flex w-2/3 items-center gap-8 rounded-md px-12 shadow-md'>
 			<Image className='my-4' src={src} alt={alt} width={24} height={24} />
